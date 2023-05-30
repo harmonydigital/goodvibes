8b4bb5c4c22488f7350d5f265e3e914faf9794ec
harmony <harmony.dgt@gmail.com>
2023-05-30 20:39:14 -0300

    ..

--- assets/components/api/api-categorias.js
+++ assets/components/api/api-categorias.js
@@ -28,6 +28,16 @@ STATUSMESA=false
 //         data.push(allDataBd)
 //     })
 // })
+
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+}
+
+function create_GUID(){
+    return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+}
+
+console.log(create_GUID());
  getApi=(container, databd)=>{
 
     console.log(databd)
